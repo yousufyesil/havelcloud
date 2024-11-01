11862cdb30159261f576c91d6155359b3ce4dd37
Yousuf Yesil <yousufyesil@icloud.com>
2024-11-01 08:21:54 +0100

    Aktualisierung der Passwortüberprüfung in password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -4,8 +4,8 @@ def password_stenght(password):
     if len(password) < 8 and password.islower() and not any(c in special_characters for c in password):
         return "Sehr Schwach"
     elif len(password) >= 8 and not password.islower() and not any(c in special_characters for c in password):
-        return "2"
+        return "Schwach"
     elif len(password) >= 8 and not password.islower() and any(c in special_characters for c in password):
-        return "3"
+        return "Mittel"
     else:
-        return "4"
+        return "Stark"
